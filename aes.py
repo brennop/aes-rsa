adc6588c219911046b8e5bd8369412a87c309ec1
brennop <brennopcordeiro@gmail.com>
2022-04-06 15:24:04 -0300

    cipher

--- aes.py
+++ aes.py
@@ -61,6 +61,8 @@ def print_matrix(matrix):
         print()
     print()
 
+def flatten(l):
+    return [x for s in l for x in s]
 
 def to_matrix(msg):
     return [[msg[i+j*4] for i in range(4)] for j in range(4)]
@@ -131,11 +133,4 @@ def cipher(block, keys):
             mix_columns(state)  # Sec 5.1.3
         add_round_key(state, keys[round])
 
-    return state
-
-
-m = [0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34]
-k = [0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c]
-ks = expand_key(k)
-c = cipher(m, ks)
-print_matrix(c)
+    return bytes(flatten(transpose(state)))
